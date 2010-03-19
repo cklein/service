ddef47d199a2c795ac4c45bf3ad7ce3aa9d07ea3
Christian Klein <c.klein@hudora.de>
2010-03-19 10:58:26 +0100

    clean up

--- config.py
+++ config.py
@@ -44,30 +44,37 @@ def create(templates):
     
     # create directory structure:
     for name, template in templates.items():
-        directory = template['directory']
-        for subdir in 'env', 'log':
-            os.makedirs(os.path.join(directory, subdir), 02755)
+        vars = template.get('vars', {})
+        try:
+            directory = template['directory']
+            for subdir in 'env', 'log':
+                os.makedirs(os.path.join(directory, subdir), 02755)
         
-        makelog(os.path.join(directory, 'log'), template['loguser'])
+            makelog(os.path.join(directory, 'log'), template.get('loguser', template.get('user')))
         
-        # environment variables:
-        uid, gid = get_ids(template['user'])
-        template['env'].update({'UID': str(uid), 'GID': str(gid)})
+            # environment variables:
+            uid, gid = get_ids(template['user'])
+            template['env'].update({'UID': str(uid), 'GID': str(gid)})
         
-        for variable, value in template['env'].items():
-            filename = os.path.join(directory, 'env', variable % template['vars'])
-            open(filename, 'w').write(value)
-            os.chmod(filename, 0644)
+            for variable, value in template['env'].items():
+                filename = os.path.join(directory, 'env', variable % vars)
+                open(filename, 'w').write(value)
+                os.chmod(filename, 0644)
         
-        # create run script:
-        fname = os.path.join(directory, 'run')
-        run = open(fname, 'w')
-        run.write('#!/bin/sh\n')
-        if 'run_prologue' in template:
-            run.write(template['run_prologue'] % template['vars'])
-        run.write('exec 2>&1\nexec envdir %s/env \\\n' % directory)
-        run.write(template['run'] % template['vars'])
-        os.chmod(fname, 0755)
+            # create run script:
+            fname = os.path.join(directory, 'run')
+            run = open(fname, 'w')
+            run.write('#!/bin/sh\n')
+            if 'run_prologue' in template:
+                run.write(template['run_prologue'] % vars)
+            run.write('exec 2>&1\nexec envdir %s/env \\\n' % directory)
+            run.write(template['run'] % vars)
+            os.chmod(fname, 0755)
+        except (KeyError, OSError), exception:
+            sys.stderr.write("Could not create service directories: %s\n" % exception)
+            # tear down
+            #print "tearing down", directory
+            pass
 
 
 def load_yaml(filename):
@@ -91,14 +98,17 @@ def main():
     parser = OptionParser()
     parser.add_option('-f', '--format', type='choice', choices=['python', 'yaml', 'json'],
                        default='python', help='Format of template file [default: %default]')
+    parser.add_option('-s', '--service', action="store_true", help="Create service directories")
     
     options, args = parser.parse_args(sys.argv)
-    if len(args) != 2:
-        parser.error('No template file given')
     
+    if len(args) != 2:
+       parser.error('No template file given')
     templates = globals().get('load_%s' % options.format)(args[1])
-    create(templates)
-
+    
+    if options.service:
+        create(templates)
+    
 
 if __name__ == '__main__':
     main()
